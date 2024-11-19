8b2f2555e8ba21155316c8918b99cd6b320e0b3f
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-11-19 11:42:26 -0800

    .

---
+++ autobot/main.go
@@ -0,0 +1,68 @@
+// Copyright 2024 Cosmos Nicolaou. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"cloudeng.io/cmdutil"
+	"cloudeng.io/cmdutil/cmdyaml"
+	"cloudeng.io/cmdutil/subcmd"
+	"cloudeng.io/macos/keychainfs"
+	"github.com/cosnicolaou/automation/devices"
+	"github.com/cosnicolaou/lutron/homeworks"
+)
+
+const cmdSpec = `name: autobot
+summary: autobot is a command line tool for interacting with home automation systems
+commands:
+  - name: control
+    summary: issue a series of commands to control/interact with a homne automation system
+    arguments:
+      - <name.op> - name of the device or controller and the operation to perform
+      - <parameters>...
+  - name: config
+    summary: query/inspect the configuration file
+    commands:
+      - name: display
+      - name: operations
+`
+
+func cli() *subcmd.CommandSetYAML {
+	cmd := subcmd.MustFromYAML(cmdSpec)
+	control := &Control{}
+	cmd.Set("control").MustRunner(control.Run, &ControlFlags{})
+	config := &Config{}
+	cmd.Set("config", "display").MustRunner(config.Display, &ConfigFlags{})
+	cmd.Set("config", "operations").MustRunner(config.Operations, &ConfigFlags{})
+
+	return cmd
+}
+
+var URIHandlers = map[string]cmdyaml.URLHandler{
+	"keychain": keychainfs.NewSecureNoteFSFromURL,
+}
+
+func init() {
+	devices.AvailableControllers = homeworks.SupportedControllers()
+	devices.AvailableDevices = homeworks.SupportedDevices()
+}
+
+var interrupt = errors.New("interrupt")
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancelCause(ctx)
+	cmdutil.HandleSignals(func() { cancel(interrupt) }, os.Interrupt)
+	err := cli().Dispatch(ctx)
+	if context.Cause(ctx) == interrupt {
+		cmdutil.Exit("%v", interrupt)
+	}
+	if err != nil {
+		cmdutil.Exit("%v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cosnicolaou/automation
+
+go 1.23.1
+
+require (
+	cloudeng.io/cmdutil v0.0.0-20241113171112-2102962e1386 // indirect
+	cloudeng.io/file v0.0.0-20241013003937-7fc58708a1c1 // indirect
+	cloudeng.io/macos v0.0.0-20241113171112-2102962e1386 // indirect
+	github.com/keybase/go-keychain v0.0.0-20231219164618-57a3676c3af6 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
